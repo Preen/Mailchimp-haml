33a67a2b8c40073b7faec3cc61106033d89b71a5
philipMannheimer <philip.mannheimer@gmail.com>
2014-08-13 18:19:43 +0200

    added style.css and removed test.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,6 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    haml: {
-      compile: {
-        files: [{
-          expand: true,
-          src: ['*.haml'],
-          dest: 'html',
-          ext: '.html'
-        }],
-        options: {
-          language: "ruby"
-        }
-      },
-    },
     inline: {
       dist: {
         src: [ 'index.html' ],
@@ -22,25 +9,14 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      // files: ['**/*.haml'],
-      // tasks: ['haml', 'inline']
       files: ['index.html'],
       tasks: ['inline']
     },   
   });
 
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  // grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-inline');
 
-  grunt.loadNpmTasks('grunt-haml');
-
-  // grunt.registerTask('test', ['jshint', 'qunit']);
-
-  // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-  grunt.registerTask('default', ['haml', 'inline']);
+  grunt.registerTask('default', ['inline']);
 
 };
